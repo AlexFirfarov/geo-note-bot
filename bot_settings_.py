088d1a2dc57f680a9d879c7712d5e3b30b3c0bf0
Alexander <Firfarov1995@gmail.com>
2020-09-04 16:44:13 +0300

    Add files via upload

---
+++ bot_settings_.py
@@ -0,0 +1,13 @@
+# Файл заглушка
+
+# Telegram settings
+TOKEN = ''
+DEFAULT_LIST_OF_PLACES_SIZE = 10
+DEFAULT_RADIUS = 500.0
+
+# Postgres settings
+USER = ''
+PASSWORD = ''
+HOST = ''
+PORT = ''
+DATABASE = ''
